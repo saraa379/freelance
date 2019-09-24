b91830cd6f49184b01ac136a1b72241c0ebbfb33
saraa379 <saraa379@yahoo.com>
2019-09-24 15:20:43 +0200

    menu is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home/Home.js';
+import Notexist from "./components/404.js"
+import Header from './components/Header.js';
+import Menu from './components/Menu.js';
+import Footer from './components/Footer.js';
 
 import {HOME_PATH} from './components/constants.js';
 
@@ -14,9 +18,13 @@ class App extends Component {
 
             <Router>
                 <div className="AppWrap">
+                    <Header></Header>
+                    <Menu></Menu>
                     <Switch>
                         <Route path={HOME_PATH} component={Home} />
+                        <Route component={Notexist} />
                     </Switch>
+                    <Footer></Footer>
                 </div>
             </Router>
 
